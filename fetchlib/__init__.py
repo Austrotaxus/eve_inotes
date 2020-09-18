4dffa679bc700c64ea8149d75ace646632765a9d
Austrotaxus <sergey.koziniy@gmail.com>
2020-09-19 01:48:17 +0300

    Initial comit with basic functionality

---
+++ fetchlib/__init__.py
@@ -0,0 +1,160 @@
+import sqlite3
+import os
+import pandas as pd
+import numpy as np
+
+from.utils import my_collection, NON_PRODUCTABLE
+
+# Your 'eve.db' path. Place it in folder to save everything as is
+EVEDB_PATH = 'eve.db'
+db = os.path.abspath(EVEDB_PATH)
+conn = sqlite3.connect(db)
+c = conn.cursor()
+
+
+def make_cached_tables():
+    types_q = """select  * from  invTypes """
+    activities_q = """select * from industryActivity"""
+    products_q = """select * from industryActivityProducts"""
+    materials_q = """select * from industryActivityMaterials"""
+    return {
+        'types': pd.read_sql_query(types_q, conn),
+        'activity': pd.read_sql_query(activities_q, conn),
+        'products': pd.read_sql_query(products_q, conn),
+        'materials':  pd.read_sql_query(materials_q, conn),
+    }
+
+CACHED_TABLES = make_cached_tables()
+
+
+def indexed_types():
+    return CACHED_TABLES['types'].set_index('typeID')
+
+
+def norm_types():
+    return CACHED_TABLES['types']
+
+
+def withdrawed_activities():
+    activity = CACHED_TABLES['activity']
+    res = activity
+    res = res[res['activityID'].isin((1,3,4,5,11))]
+    fuels = types[types['typeName'].str.contains('Fuel Block')]['typeID']
+    res=res[~res['typeID'].isin(fuels)]
+    return res
+
+
+def withdrawed_products():
+    res = CACHED_TABLES['products']
+    types = norm_types()
+    res = res[res['activityID'].isin((1,11))]
+    removes = types[types['typeName'].isin(NON_PRODUCTABLE)]['typeID']
+    res=res[~res['typeID'].isin(removes)]
+    return res
+
+
+def alterated_materials():
+    # Remove everything but 'reaction' and 'production'
+    materials = CACHED_TABLES['materials']
+    res = materials[materials['activityID'].isin([1,11])]
+    return res.set_index('typeID')
+
+
+def enrich_collection(col_df):
+    collection_df = col_df.merge(
+        norm_types(),
+        left_on='productName',
+        right_on='typeName'
+    )[['typeName','typeID','run','me','te']]
+    return collection_df
+
+
+
+def count_required(step):
+    def price_in_materials(x):
+        run_size  =  np.minimum(x.quantity,x.run)
+        jobs_required = (np.ceil(x.quantity / run_size)).astype('int32')
+        # 4 production
+        if x.activityID==1:     
+            run_price = int(np.ceil(x.quantity_materials * run_size * x.me))
+            r_req = jobs_required*run_size
+            run_price = np.maximum(run_price, run_size)
+            run_price = int(np.ceil(run_price * (jobs_required)))
+        # For reaction
+        elif x.activityID==11:
+            r_req  =  int(np.ceil((x.quantity /  x.quantity_product)))
+            run_price = x.quantity_materials * r_req
+        else:
+            raise Exception("Unknown reaction ID")
+        return run_price
+    # FIXME probably runs_required are not needed here
+    def runs_required(x):
+        run_size  =  np.minimum(x.quantity,x.run)
+        jobs_required = (np.ceil(x.quantity / run_size)).astype('int32')
+        # count for prodaction
+        if x.activityID==1:
+            r_req = jobs_required*run_size
+        elif  x.activityID==11:
+            r_req  =  int(np.ceil((x.quantity /  x.quantity_product)))
+        else:
+            r_req = 0
+        return r_req
+    run_price = step.apply(price_in_materials,axis=1)
+    runs_required = step.apply(runs_required,axis=1)
+    return run_price,runs_required
+
+
+def append_products(step):
+    return step.set_index('typeID').join(withdrawed_products().set_index('productTypeID'), rsuffix='_product', how='inner')
+
+def append_materials(step):
+    return step.set_index('typeID').join(alterated_materials(),how='inner',rsuffix='_materials')
+
+def append_prices(step):
+    return step.set_index('materialTypeID').join(
+        indexed_types() , how = 'inner', rsuffix = '_prices')
+
+
+def ultimate_decompose(product,run_size):
+    collection = enrich_collection(my_collection.to_dataframe())
+    types = norm_types()
+    assert product in types['typeName'].values, 'No such product in database!'
+    init_table = types[types['typeName']==product]
+    init_table = init_table[['typeID']]
+    init_table['quantity'] = run_size + (-run_size%my_collection.prints[product].runs)
+
+    atomic = pd.DataFrame([], columns=['typeID', 'quantity','basePrice']).set_index('typeID')
+
+    step = init_table
+    steps = [step]
+
+    while True:
+        step = step.merge(collection, on='typeID', how = 'left').fillna(value={'me':1,'te':1,'run':2**10})
+        with_products = append_products(step)
+        with_materials = append_materials(with_products)
+        with_prices = append_prices(with_materials)
+
+        step = with_prices
+
+        step['quantity'], step['runs_required'] = count_required(step)
+        step = step.groupby(step.index).sum()
+        step = step[['quantity']].reset_index().rename({'index':'typeID'}, axis='columns')
+
+        atomic = atomic.append(step[~step['typeID'].isin(withdrawed_products()['productTypeID'])])
+
+        step = step[step['typeID'].isin(withdrawed_products()['productTypeID'])]
+        steps.append(step)
+
+        if step.shape[0] == 0:
+            break
+    a = atomic[['typeID','quantity']].astype({'typeID':'int32'}).groupby('typeID').sum()
+    a = a.join(types.set_index('typeID'),lsuffix='-atom_')[['quantity','typeName']]
+    return steps, a
+
+
+def create_production_schema(product, run_size):
+    steps, ms = ultimate_decompose(product,run_size)
+    for s in steps:
+        v =  append_products(s).join(norm_types().set_index('typeID'))
+        v['runs_required'] = v['quantity']/v['quantity_product'] 
+        yield v[['typeID','typeName','quantity','runs_required',]]
